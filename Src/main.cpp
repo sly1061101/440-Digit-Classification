693a764560e7b1a206b8b8b53b5983d28916e85d
sly1061101 <sly1061101@gmail.com>
2017-11-28 17:37:51 -0600

    Initial

---
+++ Src/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  440-Digit-Classification
+//
+//  Created by Liuyi Shi on 11/28/17.
+//  Copyright © 2017 Liuyi Shi. All rights reserved.
+//
+
+#include <stdio.h>
+int main(){
+    printf("Enjoy MP4!!!\n");
+    return 0;
+}
